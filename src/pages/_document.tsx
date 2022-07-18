4578cdb5dfb6f2288da6fdbef67ef73238f86e91
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-07-18 01:09:02 -0300

    feat: apply theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,14 @@
+import { theme } from '@styles/theme';
+
 import { Html, Head, Main, NextScript } from 'next/document';
+import { ColorModeScript } from '@chakra-ui/react';
 
 export default function Document() {
    return (
       <Html>
          <Head />
          <body>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
             <Main />
             <NextScript />
          </body>
