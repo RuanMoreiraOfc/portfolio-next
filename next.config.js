d923397fc6a9fba8ff8a7e8748351edaa70833d7
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-08-22 14:25:41 -0300

    build: add @svgr/webpack

--- next.config.js
+++ next.config.js
@@ -10,6 +10,22 @@ const nextConfig = {
   images: {
     domains: ['drive.google.com', 'images.pexels.com'],
   },
+  webpack(config, { dir }) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.tsx?$/,
+      include: [dir],
+      use: [
+        'next-swc-loader',
+        {
+          loader: '@svgr/webpack',
+          options: { babel: false },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 if (process.env.DOMAIN_PARTS_COUNT === undefined) {
