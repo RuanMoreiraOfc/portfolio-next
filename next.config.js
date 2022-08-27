ca1dba71f07d2ccf09bec12449c64293dacc39a9
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-08-26 21:49:19 -0300

    chore: verify variables in webpack process

--- next.config.js
+++ next.config.js
@@ -15,6 +15,24 @@ const nextConfig = {
     ],
   },
   webpack(config, { dir }) {
+    const REQUIRED_VARIABLES = [
+      'DOMAIN_PARTS_COUNT',
+      'GITHUB_ACCESS_TOKEN',
+      'RESUME_URL_EN',
+      'RESUME_URL_PT',
+      'SELFIE_URL',
+    ];
+    const VARIABLES_UNDEFINED = REQUIRED_VARIABLES.filter(
+      (variable) => process.env[variable] === undefined,
+    );
+    if (VARIABLES_UNDEFINED.length > 0) {
+      throw new Error(
+        `\`${VARIABLES_UNDEFINED.join(' & ')}\` ${
+          VARIABLES_UNDEFINED.length === 1 ? 'was' : 'were'
+        } not defined!`,
+      );
+    }
+
     config.module.rules.push({
       test: /\.svg$/,
       issuer: /\.tsx?$/,
@@ -31,23 +49,5 @@ const nextConfig = {
     return config;
   },
 };
-
-const REQUIRED_VARIABLES = [
-  'DOMAIN_PARTS_COUNT',
-  'GITHUB_ACCESS_TOKEN',
-  'RESUME_URL_EN',
-  'RESUME_URL_PT',
-  'SELFIE_URL',
-];
-const VARIABLES_UNDEFINED = REQUIRED_VARIABLES.filter(
-  (variable) => process.env[variable] === undefined,
-);
-if (VARIABLES_UNDEFINED.length > 0) {
-  throw new Error(
-    `\`${VARIABLES_UNDEFINED.join(' & ')}\` ${
-      VARIABLES_UNDEFINED.length === 1 ? 'was' : 'were'
-    } not defined!`,
-  );
-}
 
 module.exports = nextConfig;
