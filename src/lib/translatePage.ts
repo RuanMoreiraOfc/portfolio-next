effcf5bf4f10c26eb4defb828284a876611f9b37
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-06 15:25:23 -0300

    chore: create `translatePaths` function

--- src/lib/translatePage.ts
+++ src/lib/translatePage.ts
@@ -1,6 +1,6 @@
 import type { GetStaticPropsContext } from 'next';
 
-export { translate };
+export { translate, translatePaths };
 
 const translate = async <T>(path: string, ctx: GetStaticPropsContext) => {
   try {
@@ -9,6 +9,25 @@ const translate = async <T>(path: string, ctx: GetStaticPropsContext) => {
     );
 
     return translation as T;
+  } catch (err) {
+    return null;
+  }
+};
+
+const translatePaths = async <T>(
+  paths: string[],
+  ctx: GetStaticPropsContext,
+) => {
+  try {
+    const translationEntriesPromises = paths.map(
+      (path) => import(`public/locales/${path}/${ctx.locale}.json`),
+    );
+    const translationsEntries = await Promise.all(translationEntriesPromises);
+    const translations = translationsEntries
+      .map(({ default: translation }) => translation)
+      .reduce((acc, e) => ({ ...acc, ...e }), {});
+
+    return translations as T;
   } catch (err) {
     return null;
   }
